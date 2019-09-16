81a522337d6e6301df8ed7d633568fa0f3faf719
shostak-pgs <54119632+shostak-pgs@users.noreply.github.com>
2019-09-16 20:12:00 +0300

    Delete TreasureDao.java

--- dao/TreasureDao.java
+++
@@ -1,9 +0,0 @@
-package by.htp.part02.block10.ex4.dragon_cave.dao;
-
-import java.util.List;
-
-import by.htp.part02.block10.ex4.dragon_cave.bean.Treasure;
-
-public interface TreasureDao {
-    List<Treasure> getListFromTxt(String str) throws DAOException;
-}
